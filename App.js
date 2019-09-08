f99bada5b7b5d10143d3e2f6c88e1482966493d1
jaeyp <jp.inseoul@gmail.com>
2019-09-07 23:14:09 -0400

    changed Loading.js path

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Alert } from 'react-native';
-import Loading from './Loading';
+import Loading from './components/Loading';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
+//import axios from 'axios';
+
+const API_KEY = 'c787ac53f8cd62c85720a5e465fbdc2e';
 
 export default class App extends React.Component {
     state = {
         isLoading: true,
+    }
+    getWeather = async (lat, lon) => {
+        //const { data } = await axios.get(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`);
+        //console.log(data);
     }
 	getLocation = async () => {
 		try {
@@ -17,9 +24,10 @@ export default class App extends React.Component {
             }
 
             // Get geo-location of device
-            const { coords: { altitude: alt, latitude: lat, longitude: long } } 
+            const { coords: { altitude: alt, latitude: lat, longitude: lon } } 
                 = await Location.getCurrentPositionAsync(); // destructuring assignment
-            console.log(`altitude: ${alt} latitude: ${lat} longitude: ${long}`);
+            console.log(`altitude: ${alt} latitude: ${lat} longitude: ${lon}`);
+            this.getWeather(lat, lon);
             this.setState({ isLoading: false });
 
             // Send to API and Get weather
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # react-native-weather
 Learning React Native by Building a Simple Weather App
+
+## Setup
+* Install Expo CLI
+```bash
+~$ npm install -g expo-cli
+~$ expo init weather-app
+```
+
+* Connect to git repository
+```bash
+~$ git remote add origin https://github.com/jaeyp/react-native-weather  
+~$ git pull origin master --allow-unrelated-histories  
+```
+
+* Install Expo Location API
+```bash
+~$ expo install expo-location
+```
+
+* Install axios (Promise based HTTP client)
+```bash
+~$ yarn add axios
+```
+
+## Run the app
+```bash
+~$ yarn start
+```
+
+
