f1fdbe3335b858245bb4660d44f7d3156b190192
jaeyp <jp.inseoul@gmail.com>
2019-09-08 14:37:40 -0400

    added restructuring the weather object

--- App.js
+++ App.js
@@ -12,32 +12,57 @@ export default class App extends React.Component {
         isLoading: true,
     }
     getWeather = async (lat, lon) => {
-        // destructuring assignment
+        // Get weather data from openweathermap.org - check ./assets/sample.openweathermap.json for details about destructuring assignment
         const { data: { name: city, 
                         clouds: { all: cloudiness }, 
                         main: { humidity, temp, temp_max, temp_min }, 
-                        sys: { country }, 
+                        dt: forecast, 
+                        timezone,
+                        sys: { country, sunrise, sunset },
+                        visibility,
                         weather: [{ description, main: weather }], 
                         wind: { speed: wind } } } 
             = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`);
+
         // reconstruct an weather object
-        const data = { country, city, weather, description, temp, temp_max, temp_min, humidity, cloudiness, wind };
+        const data = { country, 
+                        city, 
+                        weather, 
+                        description, 
+                        temp: {},
+                        humidity, 
+                        cloudiness, 
+                        visibility, 
+                        wind, 
+                        dt: { timezone } };
+        data.temp.current = temp;
+        data.temp.max = temp_max;
+        data.temp.min = temp_min;
+        data.dt.forecast = new Date(forecast*1000);
+        data.dt.sunrize = new Date(sunrise*1000);
+        data.dt.sunset = new Date(sunset*1000);
+        console.log(`forecasted Time:\t${data.dt.forecast.toString()} (${forecast})\n
+                    Sunrise Time:\t${data.dt.sunrize.toString()} (${sunrise})\n
+                    Sunset Time:\t${data.dt.sunset.toString()} (${sunset})`);
         console.log(data);
     }
 	getLocation = async () => {
 		try {
-            // Get device permission for location
-			let { status } = await Permissions.askAsync(Permissions.LOCATION); // destructuring assignment - 'status' is one property of the return object.
+            // Get device permission for location - Check ./assets/sample.expo.json for details
+			let { status } = await Permissions.askAsync(Permissions.LOCATION);
 			if (status !== 'granted') {
 				throw Error('Permission request was rejected.');
             }
-            // Get geo-location of device
+
+            // Get geo-location of device - Check ./assets/sample.expo.json for details
             const { coords: { altitude: alt, latitude: lat, longitude: lon }, timestamp } 
                 = await Location.getCurrentPositionAsync(); // destructuring assignment
             console.log(`altitude: ${alt} latitude: ${lat} longitude: ${lon} ts: ${timestamp}`);
+            
             // Send to API and Get weather
             this.getWeather(lat, lon);
-            // Quit Loading
+            
+            // Update Component to quit Loading screen
             this.setState({ isLoading: false });
 		} catch (e) {
 			console.log(e.message);
