a98b60c27b8144182564c727d830b1aa03da6907
jaeyp <jp.inseoul@gmail.com>
2019-09-10 00:14:12 -0400

    MOdified background

--- components/Loading.js
+++ components/Loading.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Loading() {
     return (
-        <View style={styles.container}>
+        <LinearGradient colors={['#F2C2D4', '#D3C4F2', '#6289D9']} style={styles.container}>
             <Text style={styles.text}>
-                Getting the weather
+                Loading...
             </Text>
-        </View>
+        </LinearGradient>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+        /*
         backgroundColor: '#fdf6aa',
         justifyContent: 'flex-end',
         paddingHorizontal: 30,
         paddingVertical: 100,
+        */
     },
     text: {
-        color: '#2c2c2c',
+        color: 'white',
         fontSize: 30,
+        fontWeight: 'bold'
     }
 });
